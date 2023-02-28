f3fcf33de321d9b4016d54899ddbed9c61655f3f
mdonnenfeld <mathilde.donnenfeld@sante.gouv.fr>
2023-02-28 14:39:52 +0000

    ajout renv

--- R/script.R
+++ R/script.R
@@ -12,7 +12,7 @@ source("R/functions.R", encoding = "UTF-8")
 # IMPORT DES DONNEES ---------------
 
 df <- arrow::read_parquet(
-  "individu_reg.csv",
+  "data/individu_reg.parquet",
   col_select = c("region", "aemm", "aged", "anai",
                  "catl", "cs1", "cs2", "cs3", "couple",
                  "na38", "naf08", "pnai12", "sexe",
@@ -37,7 +37,29 @@ df$sexe <- df$sexe %>%
 
 # STATS DESC  ---------------
 
+## AGE ========
+
 summarise(group_by(df, aged), n())
+
+stats_age <- df %>%
+  group_by(decennie = decennie_a_partir_annee(aged)) %>%
+  summarise(n())
+
+table_age <- gt::gt(stats_age) %>%
+  gt::tab_header(
+    title = "Distribution des âges dans notre population"
+  ) %>%
+  gt::fmt_number(
+    columns = `n()`,
+    sep_mark = " ",
+    decimals = 0
+  ) %>%
+  gt::cols_label(
+    decennie = "Tranche d'âge",
+    `n()` = "Population"
+  )
+
+## SEXE =========
 
 indicateur_stat(df %>% filter(sexe == "Homme") %>% pull(aged))
 indicateur_stat(df %>% filter(sexe == "Femme") %>% pull(aged))
